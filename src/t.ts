c48d8de506619d0cce5f8227c03b9df05efa63d5
mk360 <hamoudi191@hotmail.fr>
2022-07-10 23:28:17 +0200

    initial commit

---
+++ src/t.ts
@@ -0,0 +1,10 @@
+export default class Main extends Phaser.Scene {
+    constructor() {
+        super('test');
+    }
+
+    create() {
+        const ani = this.tweens.create({
+        })
+    }
+}
